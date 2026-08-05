146a5f9c7a9e9a9b50f2dec66fac90139ec4eb65
Ricardo Rodrigues <ricardo.rodrigues@sparkoop.com>
2026-08-04 22:59:54 -0300

    fix: corrige ordem da cascata responsiva FE-09.A.04

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,9 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import App from './App.tsx'
 import './responsive.css'
 import './responsive-stabilization.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
